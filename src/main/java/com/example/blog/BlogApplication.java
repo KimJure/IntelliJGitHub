4c13cf9aead1dcfbc7a6a7eb81bb89a37c1383ed
KimJure <sada2knuy@naver.com>
2023-12-22 02:44:49 +0900

    first commit

---
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package com.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+    @EnableJpaAuditing
+    @SpringBootApplication
+    public class MustHaveSpringBootApplication {
+        public static void main (String[] args) {
+            SpringApplication.run(MustHaveSpringBootApplication.class, args);
+        }
+    }
+}
